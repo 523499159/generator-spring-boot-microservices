216ea95266c14d1b16e34efda9c16a549b122667
Jose David Baena <josedab@gmail.com>
2016-03-23 22:09:23 -0700

    Template generator for the configuration service

--- generators/app/index.js
+++ generators/app/index.js
@@ -82,91 +82,40 @@ MicroserviceGenerator.prototype.app = function app() {
     // Micro service starter REST
     // ----------------------------
     var packageFolder = this.packageName.replace(/\./g, '/');
-    var restDir = this.baseName + '-rest/';
-    var restDirTemplate = 'microservice-starter-rest/';
-    var javaDir = restDir + 'src/main/java/' + packageFolder + '/';
-    var javaDirTemplate = restDirTemplate + 'src/main/java/package/';
-    var dockerDir = restDir + 'src/main/docker/';
-    var dockerDirTemplate = restDirTemplate + 'src/main/docker/';
-    var resourceDir = restDir + 'src/main/resources/';
-    var resourceDirTemplate = restDirTemplate + 'src/main/resources/';
-    var testDir = restDir + 'src/test/java/' + packageFolder + '/';
-    var testDirTemplate = restDirTemplate + 'src/test/java/package/';
+    var configurationServiceTemplate = 'configuration-service/';
 
-    // Docker
-    //this.template(dockerDirTemplate + 'Dockerfile', dockerDir  + 'Dockerfile', this, {});
+
+    var serviceDir = 'configuration-service/';
+    var serviceDirTemplate = 'configuration-service/';
+    var javaDir = serviceDir + 'src/main/java/' + packageFolder + '/';
+    var javaDirTemplate = serviceDirTemplate + 'src/main/java/';
+    var resourceDir = serviceDir + 'src/main/resources/';
+    var resourceConfigDir = resourceDir + 'src/main/resources/';
+    var resourceDirTemplate = serviceDirTemplate + 'src/main/resources/';
+    var resourceConfigDirTemplate = resourceDirTemplate + 'config/';
 
     // Resource
-    this.template(resourceDirTemplate + 'application.yml', resourceDir  + 'application.yml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-    //this.template(resourceDirTemplate + 'application-dev.yml', resourceDir  + 'application-dev.yml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-    //this.template(resourceDirTemplate + 'application-prod.yml', resourceDir  + 'application-prod.yml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-    this.template(resourceDirTemplate + 'bootstrap.yml', resourceDir  + 'bootstrap.yml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
+    this.template(resourceDirTemplate + 'application.properties', resourceDir  + 'application.properties', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
+    this.template(resourceConfigDirTemplate + 'eureka-service.properties', resourceConfigDir  + 'eureka-service.properties', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
+    this.template(resourceConfigDirTemplate + 'zipkin-service.properties', resourceConfigDir  + 'zipkin-service.properties', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
+    this.template(resourceConfigDirTemplate + 'zuul-service.properties', resourceConfigDir  + 'zuul-service.properties', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
+
+    // Java
+    this.template(javaDirTemplate + 'ConfigurationServiceApplication.java', javaDir + 'ConfigurationServiceApplication.java', this, {});
 
     // Test
-    //this.template(testDirTemplate + 'ApplicationTests.java', testDir + 'ApplicationTests.java', this, {});
-    this.template(testDirTemplate + 'rest/controller/HomeControllerTest.java', testDir + 'rest/controller/HomeControllerTest.java', this, {});
-    this.template(testDirTemplate + 'core/package-info.java', testDir + 'core/package-info.java', this, {});
-
-
-    // Java
-    this.template(javaDirTemplate + 'Application.java', javaDir + 'Application.java', this, {});
-    this.template(javaDirTemplate + 'config/ApplicationSettings.java', javaDir + 'config/ApplicationSettings.java', this, {});
-    this.template(javaDirTemplate + 'config/SecurityConfig.java', javaDir + 'config/SecurityConfig.java', this, {});
-    this.template(javaDirTemplate + 'config/CustomPermissionEvaluator.java', javaDir + 'config/CustomPermissionEvaluator.java', this, {});
-    this.template(javaDirTemplate + 'rest/controller/HomeController.java', javaDir + 'rest/controller/HomeController.java', this, {});
-    this.template(javaDirTemplate + 'rest/assembler/package-info.java', javaDir + 'rest/assembler/package-info.java', this, {});
-    //this.template(javaDirTemplate + 'rest/global/GlobalExceptionHandler.java', javaDir + 'rest/global/GlobalExceptionHandler.java', this, {});
-    this.template(javaDirTemplate + 'core/package-info.java', javaDir + 'core/package-info.java', this, {});
-
+    // this.template(testDirTemplate + 'rest/controller/HomeControllerTest.java', testDir + 'rest/controller/HomeControllerTest.java', this, {});
+    // this.template(testDirTemplate + 'core/package-info.java', testDir + 'core/package-info.java', this, {});
 
     // Project
-    this.template(restDirTemplate + 'pom.xml', restDir + 'pom.xml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-
-    // ----------------------------
-    // Micro service starter MODEL
-    // ----------------------------
-    var modelDir = this.baseName + '-model/';
-    var modelDirTemplate = 'microservice-starter-model/';
-    var modelJavaDir = modelDir + 'src/main/java/' + packageFolder + '/model/';
-    var modelJavaDirTemplate = modelDirTemplate + 'src/main/java/package/';
-    var modelResourceDir = modelDir + 'src/main/resources/';
-    var modelResourceDirTemplate = modelDirTemplate + 'src/main/resources/';
-    var modelTestDir = modelDir + 'src/test/java/' + packageFolder + '/model/';
-    var modelTestDirTemplate = modelDirTemplate + 'src/test/java/package/';
+    this.template(serviceDirTemplate + 'pom.xml', serviceDir + 'pom.xml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
 
 
-    // Java
-    this.template(modelJavaDirTemplate + 'package-info.java', modelJavaDir + 'package-info.java', this, {});
-
-    // Project
-    this.template(modelDirTemplate + 'pom.xml', modelDir + 'pom.xml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-
-    // ----------------------------
-    // Micro service starter IT
-    // ----------------------------
-    var itDir = this.baseName + '-it/';
-    var itDirTemplate = 'microservice-starter-it/';
-    var itResourceDir = itDir + 'src/test/resources/';
-    var itResourceDirTemplate = itDirTemplate + 'src/test/resources/';
-    var itTestDir = itDir + 'src/test/java/' + packageFolder + '/it/';
-    var itTestDirTemplate = itDirTemplate + 'src/test/java/package/';
-
-    // Java
-    this.template(itTestDirTemplate + 'IntegrationTest.java', itTestDir + 'IntegrationTest.java', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-
-    // Project
-    this.template(itDirTemplate + 'pom.xml', itDir + 'pom.xml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-
-    // Resource
-    this.template(itResourceDirTemplate + 'mock1.json', itResourceDir + 'mock1.json', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
 
 
     // ----------------------------
     // Micro service starter MAIN
     // ----------------------------
-    this.template('pom.xml', 'pom.xml', this, { 'interpolate': /<%=([\s\S]+?)%>/g });
-    this.template('.npmignore', '.gitignore', this, {});
-
     this.config.set('packageName', this.packageName);
     this.config.set('packageFolder', packageFolder);
 };
